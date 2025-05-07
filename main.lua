8398e5117bbe33b0d2fed0258935cf24cdfda205
Tahir Dibrov II <tahirdibirovsecond@gmail.com>
2025-05-07 15:04:29 +0400

    multiline string

---
+++ main.lua
@@ -0,0 +1,21 @@
+-- This is singleline bullshit!
+
+--[[
+This is multiline bullshit
+]]
+
+local some_variable = "Fuck it!!";
+name = "Alice "
+surname = "Smith"
+fullname = name .. surname
+local multiline_string  = [[Tahir 
+Dibirov]]
+print(multiline_string)
+
+do
+    local variable = "Fuck it in self-made scope!"
+    print(variable)
+end
+
+some_fuckin_number = 56
+print(tostring(some_fuckin_number))
